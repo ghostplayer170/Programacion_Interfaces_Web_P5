5329cb065819ed7fd4ecd261727912277bc0891b
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-05-12 22:58:45 +0200

    first commit 1

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -11,6 +11,14 @@ export const handler: Handlers = {
   async GET(_req: Request, ctx: FreshContext<unknown, Data>) {
     const cookies = getCookies(_req.headers);
     const projects: project[] = JSON.parse(cookies.projects);
+    if (projects.length === 0) {
+      return new Response("", {
+        status: 303,
+        headers: {
+          "Location": "/_404",
+        },
+      });
+    }
     return await ctx.render({ projects });
   },
 };
